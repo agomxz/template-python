9dc2bfa7a03bb1135a7f5c33889fc754320e06aa
Efren A. Rosales Gomez <e.rosales@traxion.global>
2023-10-29 17:57:46 -0600

    Added CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from api.api_v1.api import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI( title='TEAMS')
+app = FastAPI(title="TEAMS")
 
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
